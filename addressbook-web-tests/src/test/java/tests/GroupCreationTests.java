1c98a33a8489d4397348e94f05bc0c9407afe958
sh.olja <sh.olja@gmail.com>
2020-11-16 22:50:55 +0200

    lesson 3.5 and 3.6

--- addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
@@ -11,7 +11,7 @@ public class GroupCreationTests extends TestBase{
         app.getGroupHelper()
                 .initGroupCreation("new");
         app.getGroupHelper()
-                .fillGroupForm(new GroupData("test1", "test2", "test3"));
+                .fillGroupForm(new GroupData("test1", "test2", null));
         app.getGroupHelper()
                 .submitGroupCreation("submit");
         app.getGroupHelper()
