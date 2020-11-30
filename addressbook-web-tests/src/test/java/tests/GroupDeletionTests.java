3c6b49dd2bb47082d0097f7dc0378360c70012e5
sh.olja <sh.olja@gmail.com>
2020-11-30 23:15:02 +0200

    lesson 5.2 code refactoring

--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -2,19 +2,25 @@ package tests;
 
 import model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase{
 
+    @BeforeMethod
+    public void ensurePreconditions(){
+        app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
+    }
+
+
     @Test
     public void testGroupDeletion() throws Exception {
-        app.getNavigationHelper()
-                .gotoGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()){
-            app.getGroupHelper().createGroup(new GroupData("test3", null, null));
-        }
+
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper()
                 .selectGroup(before.size() - 1);
